1e98037d9df5ae3d05cbc2877c695ee137deba5e
Ian Yin <yin.ian093@gmail.com>
2020-07-16 15:30:14 +1000

    Some js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-let result = 0;
+let result = 10;
+let tempNum1 = 0;
+let tempNum2 = 0;
+let input = [false, false];
+let index = 0;
+let display = document.getElementById("result");
+
+
 
 function equal () {
-    console.log(result);
+    display.innerHTML = result;
 }
 
 document.getElementById('equal').addEventListener("click", equal);
